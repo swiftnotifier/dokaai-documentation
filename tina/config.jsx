7de25f535c8f5e67bd29b3a987c011221940fbde
Atul Joshi <atuljoshii@nthexam.com>
2025-07-24 21:36:46 +0530

    Removed old action

--- tina/config.jsx
+++ tina/config.jsx
@@ -182,6 +182,7 @@ export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   token: process.env.TINA_TOKEN,
   build: {
+    basePath: "dokaai-documentation",
     outputFolder: "admin",
     publicFolder: "static",
   },
